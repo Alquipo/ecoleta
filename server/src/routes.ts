1f9ecc261baede97cb9d4efc22b9edac713b33ed
Alquipo Neto <alquiponeto@outlook.com.br>
2020-06-06 00:41:01 -0300

    Ajuste bug que add .0 no final do whatsapp

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -26,7 +26,7 @@ routes.post(
         body: Joi.object().keys({
             name: Joi.string().required(),
             email: Joi.string().required().email(),
-            whatsapp: Joi.number().required(),
+            whatsapp: Joi.string().required(),
             latitude: Joi.number().required(),
             longitude: Joi.number().required(),
             city: Joi.string().required(),
